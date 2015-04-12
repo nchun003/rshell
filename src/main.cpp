0254b1b861f1cbaf835185d8c83f9a0c6f0caf55
Nichelle Chung <nchun003@hammer.cs.ucr.edu>
2015-04-11 19:56:04 -0700

    starting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-
+	std::string usrin;
+	std::getline(std::cin, usrin);
 	return 0;	
 }

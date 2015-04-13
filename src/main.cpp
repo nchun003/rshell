e5a1e106317eeff7d7b34080c9ba68e018cc57bf
Nichelle Chung <nchun003@hammer.cs.ucr.edu>
2015-04-13 15:20:59 -0700

    fixing parsing

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,21 @@ int main(int argc, char **argv)
 	std::getline(std::cin,usrin);		//convert to cstring for parsing
 	char *cstr = new char [usrin.length()+1];
 	std::strcpy (cstr, usrin.c_str());
-	char *comm_1 = strtok(cstr, "||");
-	while(comm_1 != NULL)
-	{
-		std::cout << comm_1 << std::endl;
-		comm_1 = strtok(NULL, "||");
-	}
-	char *comm_2 = strtok(cstr, "&&");
-	while(comm_2 != NULL)
-	{
-		std::cout << comm_2 << std::endl;
-		comm_2 = strtok(NULL, "&&");
-	}
-	char *comm_3 = strtok(cstr, ";");
-	while(comm_3 != NULL)
-	{
-		std::cout << comm_3 << std::endl;
-		comm_3 = strtok(NULL, ";");
-	}
-
+	std::string command;
+	char **arg_list;
+	int size = 0; 
+	int cap = 0; 
+	char *comm_1 = strtok(cstr, " ");
+        while(comm_1 != NULL)
+        {
+		if (comm_1=="||") {}
+		else if (comm_1=="&&") {}
+		else if (comm_1 == ";"){}
+		command=comm_1;
+		while(
+                std::cout << comm_1 << std::endl;
+                comm_1 = strtok(NULL, " ");
+        }
 
 	return 0;	
 }

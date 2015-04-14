56e30217accbf65642ef1be79fedb058653c2989
Nichelle Chung <nchun003@hammer.cs.ucr.edu>
2015-04-13 19:01:55 -0700

    started exec function

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <cstring>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
-void findconnectors(char *token)
+
+void exec(char **argv)
+{
+	int pid = fork();
+	if(pid == -1){
+		perror("ERROR!");
+		exit(1);
+	}
+	else if(pid == 0)			//Child process
+	{
+		std::cout << "Child Process";
+		if(execvp(argv[0], argv))
+		{
+			perror("No error.");
+		}
+		exit(1);			//Child killed when done with task
+	}
+	else if(pid > 0)
+	{
+		if(wait(0) == -1)		//Waits for child process to finish
+		{
+			perror("Wait error!");
+		}
+	}
+
+}
+
+void findconnectors(char *token)		//Checks if a token is a connector
 {
 	std::string sor = "||";
 	char *orr = new char [sor.length()+1];
@@ -17,13 +47,15 @@ void findconnectors(char *token)
 	std::cout << "token:" << token << " or:" << orr << std::endl;
 	if(*token == *orr)
 	{
-		
+		std::cout << "OR!" << std::endl;	
 	} 
 	else if(*token == *andd)
 	{
+		std::cout << "AND!" << std::endl;
 	}
 	else if(*token == *coll)
 	{
+		std::cout << "COLON!" << std::endl;
 	}
 
 }

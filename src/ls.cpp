92c4c0248cf8bd6f11828c461196e9df5b304ec0
Nichelle Chung <nchun003@hammer.cs.ucr.edu>
2015-04-28 22:40:47 -0700

    starting ls

---
+++ src/ls.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
+
